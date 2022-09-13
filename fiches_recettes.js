13ab083b07c1b827ef7dd68e04d9838d2a168aed
NJL <naeljl@protonmail.com>
2022-09-13 12:39:41 +0200

    ajout fiche recette + mise en forme CSS

---
+++ fiches_recettes.js
@@ -0,0 +1,35 @@
+// VARIABLES DU HTML
+
+let name = document.getElementById('name');
+let temps = document.getElementById('temps');
+let ingredients = document.getElementById('ingredients');
+let instructions = document.getElementById('instructions');
+
+// CONSTRUCTOR
+
+class FicheRecette {
+    constructor(name) {
+        this._name = name;
+        this._temps = temps;
+        this._ingredients = ingredients;
+        this._instructions = instructions;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get temps() {
+        return this._temps;
+    }
+
+    get ingredients() {
+        return this._ingredients;
+    }
+
+    get instructions() {
+        return this._instructions;
+    }
+}
+
+// INSTANCES
